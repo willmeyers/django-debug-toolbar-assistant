f21fe0fd5af3c6c841dcc6a91503401bffe06284
Will Meyers <will@willmeyers.net>
2025-05-20 12:40:38 -0400

    Upgrade package

--- django_debug_toolbar_assistant_panel/panel.py
+++ django_debug_toolbar_assistant_panel/panel.py
@@ -1,7 +1,6 @@
 from debug_toolbar.panels import Panel
 from django.db import connection
 from django.templatetags.static import static
-from .gemini import query_gemini
 
 class AssistantPanel(Panel):
     title = "Assistant"
@@ -13,12 +12,3 @@ class AssistantPanel(Panel):
         return [
             static("__debug__/djdt_assistant_panel/js/panel.js")
         ]
-
-    def generate_stats(self, request, response):
-        queries = connection.queries
-        self.record_stats({
-            "queries": [
-                {"sql": q["sql"], "time": q.get("time", "?")}
-                for q in queries
-            ]
-        })
--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name="django-debug-toolbar-assistant",
     version="0.1",
-    packages=["django_debug_toolbar_assistant_panel"],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "django>=3.2",
         "django-debug-toolbar>=4.0",
         "google-cloud-aiplatform>=1.40",
+        "markdown",
     ],
+    description="A Django Debug Toolbar panel for AI-assisted debugging using Gemini.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Will Meyers",
+    author_email="mail@willmeye.rs",
+    url="https://github.com/willmeyers/django-debug-toolbar-assistant",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
